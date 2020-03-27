1310d71734d38a4f417123f3528c4ac7914349d3
Cosmas Gikunju <gikunjucosmas@gmail.com>
2020-03-27 21:30:25 +0300

    About Page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import './About.scss';
+
 function About() {
-	return <div>About Page</div>;
+	return (
+		<div className='about'>
+			<h1 className='heading--sub'>About Bookly</h1>
+			<p className='text--grey u-margin-top-medium'>
+				Bookly helps you search for Books from the Google Books API
+			</p>
+			<p className='text--grey u-margin-top-small'>
+				Open Source and available on github at:{' '}
+				<span className='about--link'>
+					https://github.com/ItsCosmas/bookly
+				</span>
+			</p>
+		</div>
+	);
 }
 
 export default About;
