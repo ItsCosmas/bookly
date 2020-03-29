6e1f75346975e2fc6e2556ee1ab1e3bcd58fead5
Cosmas Gikunju <gikunjucosmas@gmail.com>
2020-03-29 17:06:54 +0300

    Added a no image thumbnail

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import './BookCard.scss';
 
-import default_thumbnail from '../../assets/book.jpeg';
+import default_thumbnail from '../../assets/no_image.png';
 
 function BookCard(props) {
 	let { title, imageLinks, authors } = props.book;
